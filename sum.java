c22a77c67241862eee43d14a3c889a45c3ba5caf
naveena31 <naveena22896@gmail.com>
2017-06-10 10:02:23 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class sum
+{
+	public static void main(String args[])
+	{
+		int i,sum=0;
+		for(i=0;i<100;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println("the sum is" +sum);
+	}
+}
